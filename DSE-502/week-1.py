d2fb22a6e9349e0ad27f62cb7516148966ccd0c1
tbk81 <dev@tbk81.com>
2026-09-01 05:06:10 -0700

    init commit

---
+++ DSE-502/week-1.py
@@ -0,0 +1,27 @@
+"""
+What to submit: In a document (submit in .doc, .docx, or .PDF formats; please do not
+submit other formats such as Apple’s Pages), paste screen captures showing both
+command and output after each command, and brieﬂy describe (one or two sentences
+max) what happens in each command and its respective outputs.
+"""
+
+print("Hello, world")
+print("Hello", 'world!')
+print("hello" + 'world!')
+print('UMaine' * 3)
+print('1' + '3')
+# print('2'+ 5)
+print(4.4)
+print(2 + 5)
+print(3.0 + 6.0)
+print("1" + "4")
+print("2 + 9 = ", 2 + 9)
+print (2 * 3)
+print (2 ** 3)
+print (7 / 2)
+print (7 // 2)
+print (1 // 3)
+print (-7 / 2)
+print (-7 // 2)
+print (-7.0 // 2)
+print (-7 // 2.0)
