3f1e0c94fead13486b03b26af39d2af811627bdf
MBP <dev@tbk81.com>
2026-09-08 12:54:36 -0700

    DSE 502 assignment 2

---
+++ DSE-502/week-2.py
@@ -0,0 +1,18 @@
+# import tkinter
+# tkinter._test()
+
+from turtle import *
+
+screen = Screen()
+
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+
+screen.exitonclick()
+
+
