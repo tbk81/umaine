65723aa9067d5a3516ca23a397c131fc492f2ccf
MBP <dev@tbk81.com>
2026-09-09 11:03:29 -0700

    DSE 502 assignment 2

--- DSE-502/week-2.py
+++ DSE-502/week-2.py
@@ -1,61 +1,87 @@
-from turtle import *
+from random import randint
+from turtle import Turtle, Screen
+import random
 
 screen = Screen()
+screen.title("Welcome to the Geometrical (Geo) Turtle")
 screen.setup(width=750, height=750)
 
-def move_turtle():
-    penup()
-    hideturtle()
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple", "cyan", "coral", "aquamarine", "DeepPink", "DarkViolet"]
 
-def draw_square():
-    move_turtle()
-    goto(x=-250, y=200)
-    pendown()
-    for i in range(4):
-        forward(100)
-        left(90)
+class GeoTurtle(Turtle):
 
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.speed(10)
 
-def draw_rectangle():
-    move_turtle()
-    goto(x=-100, y=200)
-    pendown()
-    for i in range(2):
-        forward(200)
-        left(90)
-        forward(100)
-        left(90)
+    def move_turtle(self):
+        self.penup()
+        self.hideturtle()
 
-
-def draw_triangle():
-    move_turtle()
-    goto(x=150, y=200)
-    pendown()
-    for i in range(3):
-        forward(100)
-        left(120)
+    def draw_square(self):
+        self.color(random.choice(COLORS))
+        self.fillcolor(random.choice(COLORS))
+        self.width(randint(1, 8))
+        self.move_turtle()
+        self.goto(x=-250, y=200)
+        self.pendown()
+        self.begin_fill()
+        for i in range(4):
+            self.forward(100)
+            self.left(90)
+        self.end_fill()
 
 
-def draw_A():
-    color('red')
-    width(5)
-    move_turtle()
-    goto(x=100, y=0)
-    pendown()
-    left(120)
-    forward(200)
-    left(120)
-    forward(200)
-    right(180)
-    forward(100)
-    right(60)
-    forward(100)
+    def draw_rectangle(self):
+        self.color(random.choice(COLORS))
+        self.fillcolor(random.choice(COLORS))
+        self.width(randint(1, 8))
+        self.move_turtle()
+        self.goto(x=-100, y=200)
+        self.pendown()
+        self.begin_fill()
+        for i in range(2):
+            self.forward(200)
+            self.left(90)
+            self.forward(100)
+            self.left(90)
+        self.end_fill()
+
+    def draw_triangle(self):
+        self.color(random.choice(COLORS))
+        self.fillcolor(random.choice(COLORS))
+        self.width(randint(1, 8))
+        self.move_turtle()
+        self.goto(x=150, y=200)
+        self.pendown()
+        self.begin_fill()
+        for i in range(3):
+            self.forward(100)
+            self.left(120)
+        self.end_fill()
 
 
-draw_square()
-draw_rectangle()
-draw_triangle()
-draw_A()
+    def draw_A(self):
+        self.color(random.choice(COLORS))
+        self.width(randint(1, 8))
+        self.move_turtle()
+        self.goto(x=100, y=0)
+        self.pendown()
+        self.left(120)
+        self.forward(200)
+        self.left(120)
+        self.forward(200)
+        self.goto(x=-50, y=86.6)
+        self.left(120)
+        self.forward(100)
+
+
+geoTurtle = GeoTurtle()
+geoTurtle.draw_square()
+geoTurtle.draw_rectangle()
+geoTurtle.draw_triangle()
+geoTurtle.draw_A()
 
 screen.exitonclick()
 
