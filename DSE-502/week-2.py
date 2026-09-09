f0e755770a014b3c6f66e8d727f2b3d521c295ff
tbk81 <dev@tbk81.com>
2026-09-08 21:09:03 -0700

    DSE 502 assignment 2

--- DSE-502/week-2.py
+++ DSE-502/week-2.py
@@ -1,17 +1,61 @@
-# import tkinter
-# tkinter._test()
-
 from turtle import *
 
 screen = Screen()
+screen.setup(width=750, height=750)
 
-forward(100)
-left(90)
-forward(100)
-left(90)
-forward(100)
-left(90)
-forward(100)
+def move_turtle():
+    penup()
+    hideturtle()
+
+def draw_square():
+    move_turtle()
+    goto(x=-250, y=200)
+    pendown()
+    for i in range(4):
+        forward(100)
+        left(90)
+
+
+def draw_rectangle():
+    move_turtle()
+    goto(x=-100, y=200)
+    pendown()
+    for i in range(2):
+        forward(200)
+        left(90)
+        forward(100)
+        left(90)
+
+
+def draw_triangle():
+    move_turtle()
+    goto(x=150, y=200)
+    pendown()
+    for i in range(3):
+        forward(100)
+        left(120)
+
+
+def draw_A():
+    color('red')
+    width(5)
+    move_turtle()
+    goto(x=100, y=0)
+    pendown()
+    left(120)
+    forward(200)
+    left(120)
+    forward(200)
+    right(180)
+    forward(100)
+    right(60)
+    forward(100)
+
+
+draw_square()
+draw_rectangle()
+draw_triangle()
+draw_A()
 
 screen.exitonclick()
 
